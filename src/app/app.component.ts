85620f8555276dabfe0d54fdecc2d45a1eaa3d51
callanbr <chrisallan1992@icloud.com>
2018-07-09 13:42:56 -0700

    Christopher chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { EventsService } from "./events.service";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
   listName: string;
