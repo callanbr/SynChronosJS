24e643ad537006c1999ab248a2307350292dc7f6
Megreb812 <39529989+Megreb812@users.noreply.github.com>
2018-07-19 20:33:05 -0700

    Restore functionality to login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthService, GoogleLoginProvider } from "angular5-social-login";
-declare var gapi: any;
+import {LoginService} from "./login.service";
 
 @Component({
   selector: "app-login",
@@ -8,7 +8,8 @@ declare var gapi: any;
   styleUrls: ["./login.component.css"]
 })
 export class loginComponent implements OnInit {
-  constructor(private socialAuthService: AuthService) {}
+  constructor(private socialAuthService: AuthService, private loginService: LoginService) {}
+
 
   public socialSignIn(socialPlatform: string) {
     let socialPlatformProvider;
@@ -16,37 +17,22 @@ export class loginComponent implements OnInit {
       socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
     }
 
+
     this.socialAuthService.signIn(socialPlatformProvider).then(userData => {
-      console.log(socialPlatform + " sign in data : ", userData);
-      // Now sign-in with userData
+      this.loginService.loginUser(userData).subscribe(data => {
+        localStorage.setItem('currentUser',JSON.stringify({ token: data }));
+      }
+    );
+      (<any>window).mything = this.loginService;
       console.log("This is a token:" + userData.token);
     });
   }
 
-  
+
   public socialSignOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function() {
-      console.log("User signed out.");
-    });
-    alert("Google Logout.");
+    this.loginService.logout();
   }
 
-  //  loadClient(): Promise<any> {
-  //   return new Promise((resolve, reject) => {
-  //       this.zone.run(() => {
-  //              gapi.load('client', {
-  //                  callback: resolve,
-  //                  onerror: reject,
-  //                  timeout: 1000, // 5 seconds.
-  //                  ontimeout: reject
-  //              });
-  //       });
-  //  });
-
-  //  this.zone.run(() => {
-  //   // gapi.load
-  // });
 
   ngOnInit() {}
 }
