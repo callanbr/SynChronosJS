52f8eb409d2edebd8722a9a0704793267d3f0066
callanbr <chrisallan1992@icloud.com>
2018-08-08 21:27:33 -0700

    css

--- src/js/myWebsite.js
+++ src/js/myWebsite.js
@@ -45,3 +45,16 @@
 
   $(window).scroll(navbarCollapse);
 })(jQuery);
+
+
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("groups").style.display = "none";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("groups").style.display = "block";
+}
